6ebb08b2368aa4e32ceeb63202ab3304da4a214d
hainguyen <113692029+cdoge25@users.noreply.github.com>
2022-11-16 23:21:58 +0700

    Tao Splash Screen (#2)

--- app/src/main/java/com/nhom6/davidsonfurniture/Activities/MainActivity.java
+++ app/src/main/java/com/nhom6/davidsonfurniture/Activities/MainActivity.java
@@ -1,16 +1,35 @@
 package com.nhom6.davidsonfurniture.Activities;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.nhom6.davidsonfurniture.R;
+import com.nhom6.davidsonfurniture.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMER=2000;
+
+    ActivityMainBinding binding;
+    ImageView imvLogoSplash;
+    Animation fadeInAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        fadeInAnim = AnimationUtils.loadAnimation(this,R.anim.fade_in_anim);
+        binding.imvLogoSplash.setAnimation(fadeInAnim);
     }
 }
