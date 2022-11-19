eabad4b9907706d0dd3bf9696ae030fea8a8d2cf
cdoge25 <haindt20411@st.uel.edu.vn>
2022-11-19 15:27:36 +0700

    Code giao dien man hinh dang nhap

---
+++ app/src/main/java/com/nhom6/davidsonfurniture/Activities/ForgetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.nhom6.davidsonfurniture.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nhom6.davidsonfurniture.R;
+
+public class ForgetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
--- app/src/main/java/com/nhom6/davidsonfurniture/Activities/LoginActivity.java
+++ app/src/main/java/com/nhom6/davidsonfurniture/Activities/LoginActivity.java
@@ -3,6 +3,7 @@ package com.nhom6.davidsonfurniture.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.nhom6.davidsonfurniture.R;
 
@@ -11,6 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_login);
     }
 }
