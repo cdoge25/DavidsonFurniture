b02896f253e8e2d080b950427683e12f07f3b699
anh20411 <anhntt20411@st.uel.edu.vn>
2022-11-20 21:44:38 +0700

    add giaodiensanphamphobien

--- app/src/main/java/com/nhom6/davidsonfurniture/Activities/NewProductActivity.java
+++ app/src/main/java/com/nhom6/davidsonfurniture/Activities/NewProductActivity.java
@@ -2,15 +2,67 @@ package com.nhom6.davidsonfurniture.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
+import com.nhom6.davidsonfurniture.Adapters.ProductAdapter;
+import com.nhom6.davidsonfurniture.Models.Product;
 import com.nhom6.davidsonfurniture.R;
+import com.nhom6.davidsonfurniture.databinding.ActivityNewProductBinding;
+
+import java.util.ArrayList;
 
 public class NewProductActivity extends AppCompatActivity {
+
+    ActivityNewProductBinding binding;
+    ProductAdapter adapter;
+    ArrayList<Product> productList;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_product);
+        //setContentView(R.layout.activity_new_product);
+        binding = ActivityNewProductBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = getIntent();
+
+        loadData();
+        addEvent();
+    }
+
+    private void addEvent() {
+        binding.gvNewProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(NewProductActivity.this, MainActivity.class);
+                intent.putExtra("Name", productList.get(i).getProductName());
+                intent.putExtra("Image", productList.get(i).getProductThumb());
+                intent.putExtra("Price", productList.get(i).getProductPrice());
+                intent.putExtra("Rate", productList.get(i).getProductRate());
+                intent.putExtra("Category", productList.get(i).getProductCategory());
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void loadData() {
+        productList = new ArrayList<>();
+        productList.add(new Product(R.drawable.img_sofabang_alice, "ALICE", "Sofa Bằng", "4.7", 4500000));
+        productList.add(new Product(R.drawable.img_ghelamviec_qile, "QILE", "Ghế làm việc", "4.7", 1590000));
+        productList.add(new Product(R.drawable.img_bancafe_luki, "LUKI", "Bàn Cafe", "4.7", 1350000));
+        productList.add(new Product(R.drawable.img_banan_honey,"HONEY", "Bàn ăn", "4.7", 2250000));
+        productList.add(new Product(R.drawable.img_giuongngu_lullaby, "LULLABY", "Giường gỗ", "4.7", 10500000));
+        productList.add(new Product(R.drawable.img_guongdeban_coba, "COBA", "Gương để bàn", "4.7", 1220000));
+        productList.add(new Product(R.drawable.img_guongtoanthan_patax, "PATAX", "Gương toàn thân", "4.7",2690000));
+        productList.add(new Product(R.drawable.img_guongtoanthan_tama, "TAMA", "Gương toàn thân", "4.7",2690000));
+
+        adapter = new ProductAdapter(NewProductActivity.this, R.layout.item_new_product,  productList);
+        binding.gvNewProduct.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/nhom6/davidsonfurniture/Models/Product.java
@@ -0,0 +1,58 @@
+package com.nhom6.davidsonfurniture.Models;
+
+public class Product {
+
+    public Product(int productThumb, String productName, String productCategory, String productRate, double productPrice) {
+        this.productThumb = productThumb;
+        this.productName = productName;
+        this.productCategory = productCategory;
+        this.productRate = productRate;
+        this.productPrice = productPrice;
+    }
+
+    public int getProductThumb() {
+        return productThumb;
+    }
+
+    public void setProductThumb(int productThumb) {
+        this.productThumb = productThumb;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductRate() {
+        return productRate;
+    }
+
+    public void setProductRate(String productRate) {
+        this.productRate = productRate;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    int productThumb;
+    String productName, productCategory, productRate;
+    double productPrice;
+
+
+}
