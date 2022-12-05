c618a1a7c5a1d9d26f37aebdaa0455de244e6e62
cdoge25 <haindt20411@st.uel.edu.vn>
2022-12-05 13:23:03 +0700

    fix giao dien san pham

--- app/src/main/java/com/nhom6/davidsonfurniture/Activities/CategoriesProductActivity.java
+++ app/src/main/java/com/nhom6/davidsonfurniture/Activities/CategoriesProductActivity.java
@@ -110,81 +110,81 @@ public class CategoriesProductActivity extends AppCompatActivity {
         if (Objects.equals(category, "sofa")){
             productList = new ArrayList<>();
             productList.add(new Product(R.drawable.img_sofabang_anastasia,"ANASTASIA", "Sofa Băng", "4.7", 8500000));
-            productList.add(new Product(R.drawable.img_sofabang_alice,"ALICE", "Sofa băng", "4.7", 4500000));
-            productList.add(new Product(R.drawable.img_sofabang_jasmin,"JASMIN", "Sofa Băng", "4.7", 10850000));
-            productList.add(new Product(R.drawable.img_sofabang_aurora,"AURORA", "Sofa băng", "4.7", 3290000));
-            productList.add(new Product(R.drawable.img_sofabang_elena,"ELENA", "Sofa Băng", "4.7", 5850000));
-            productList.add(new Product(R.drawable.img_sofabang_muscle,"ALICE", "Sofa băng", "4.7", 5850000));
+            productList.add(new Product(R.drawable.img_sofabang_alice,"ALICE", "Sofa băng", "4.5", 4500000));
+            productList.add(new Product(R.drawable.img_sofabang_jasmin,"JASMIN", "Sofa Băng", "4.3", 10850000));
+            productList.add(new Product(R.drawable.img_sofabang_aurora,"AURORA", "Sofa băng", "4.2", 3290000));
+            productList.add(new Product(R.drawable.img_sofabang_elena,"ELENA", "Sofa Băng", "4.1", 5850000));
+            productList.add(new Product(R.drawable.img_sofabang_muscle,"ALICE", "Sofa băng", "4.8", 5850000));
             binding.txtCategoryName.setText("Sofa");
         }
         else if (Objects.equals(category, "ghe")){
             productList = new ArrayList<>();
-            productList.add(new Product(R.drawable.img_ghelamviec_qile,"QILE", "Ghế làm việc", "4.7", 1590000));
+            productList.add(new Product(R.drawable.img_ghelamviec_qile,"QILE", "Ghế làm việc", "4.0", 1590000));
             productList.add(new Product(R.drawable.img_ghean_astu,"ASTU", "Ghế ăn", "4.7", 594000));
-            productList.add(new Product(R.drawable.img_ghean_noven,"NOVEN", "Ghế ăn", "4.7", 2850000));
-            productList.add(new Product(R.drawable.img_ghebanh_moana,"MOANA", "Ghế bành", "4.7", 7950000));
-            productList.add(new Product(R.drawable.img_ghedon_michau,"MICHAU", "Ghế đôn", "4.7", 4250000));
-            productList.add(new Product(R.drawable.img_ghelamviec_miti,"MITI", "Ghế làm việc", "4.7", 2490000));
+            productList.add(new Product(R.drawable.img_ghean_noven,"NOVEN", "Ghế ăn", "4.2", 2850000));
+            productList.add(new Product(R.drawable.img_ghebanh_moana,"MOANA", "Ghế bành", "4.2", 7950000));
+            productList.add(new Product(R.drawable.img_ghedon_michau,"MICHAU", "Ghế đôn", "4.3", 4250000));
+            productList.add(new Product(R.drawable.img_ghelamviec_miti,"MITI", "Ghế làm việc", "4.6", 2490000));
             binding.txtCategoryName.setText("Ghế");
         }
         else if (Objects.equals(category, "ban")){
             productList = new ArrayList<>();
-            productList.add(new Product(R.drawable.img_banan_honey,"HONEY", "Bàn ăn", "4.7", 1150000));
-            productList.add(new Product(R.drawable.img_banlamviec_builder,"BUILDER", "Bàn làm việc", "4.7", 2850000));
-            productList.add(new Product(R.drawable.img_bancafe_luki,"LUKI", "Bàn cafe", "4.7", 1350000));
+            productList.add(new Product(R.drawable.img_banan_honey,"HONEY", "Bàn ăn", "3.7", 1150000));
+            productList.add(new Product(R.drawable.img_banlamviec_builder,"BUILDER", "Bàn làm việc", "3.9", 2850000));
+            productList.add(new Product(R.drawable.img_bancafe_luki,"LUKI", "Bàn cafe", "4.5", 1350000));
             productList.add(new Product(R.drawable.img_banan_lehi,"LEHI", "Bàn ăn", "4.7", 5490000));
-            productList.add(new Product(R.drawable.img_bancafe_mushroom,"MUSHROOM", "Bàn cafe", "4.7", 3750000));
-            productList.add(new Product(R.drawable.img_bantrangdiem_mbinas,"MBINAS", "Bàn trang điểm", "4.7", 2750000));
+            productList.add(new Product(R.drawable.img_bancafe_mushroom,"MUSHROOM", "Bàn cafe", "4.4", 3750000));
+            productList.add(new Product(R.drawable.img_bantrangdiem_mbinas,"MBINAS", "Bàn trang điểm", "4.3", 2750000));
             binding.txtCategoryName.setText("Bàn");
         }
         else if (Objects.equals(category, "guong")){
             productList = new ArrayList<>();
-            productList.add(new Product(R.drawable.img_guongnho_arou,"AROU", "Gương nhỏ", "4.7", 1250000));
-            productList.add(new Product(R.drawable.img_guongnho_mibo,"MIBO", "Gương nhỏ", "4.7", 1250000));
-            productList.add(new Product(R.drawable.img_guongtoanthan_tama,"TAMA", "Gương toàn thân", "4.7", 2690000));
-            productList.add(new Product(R.drawable.img_guongdeban_coba,"COBA", "Gương để bàn", "4.7", 1220000));
-            productList.add(new Product(R.drawable.img_guongtoanthan_patax,"PATAX", "Gương toàn thân", "4.7", 2690000));
-            productList.add(new Product(R.drawable.img_guongdeban_utu,"UTU", "Gương để bàn", "4.7", 390000));
+            productList.add(new Product(R.drawable.img_guongnho_arou,"AROU", "Gương nhỏ", "4.5", 1250000));
+            productList.add(new Product(R.drawable.img_guongnho_mibo,"MIBO", "Gương nhỏ", "4.6", 1250000));
+            productList.add(new Product(R.drawable.img_guongtoanthan_tama,"TAMA", "Gương toàn thân", "4.0", 2690000));
+            productList.add(new Product(R.drawable.img_guongdeban_coba,"COBA", "Gương để bàn", "3.9", 1220000));
+            productList.add(new Product(R.drawable.img_guongtoanthan_patax,"PATAX", "Gương toàn thân", "4.1", 2690000));
+            productList.add(new Product(R.drawable.img_guongdeban_utu,"UTU", "Gương để bàn", "4.2", 390000));
             binding.txtCategoryName.setText("Gương");
         }
         else if (Objects.equals(category, "den")){
             productList = new ArrayList<>();
-            productList.add(new Product(R.drawable.img_denban_feeca,"FEECA", "Đèn bàn", "4.7", 449000));
-            productList.add(new Product(R.drawable.img_densan_logly,"LOGLY", "Đèn sàn", "4.7", 990000));
+            productList.add(new Product(R.drawable.img_denban_feeca,"FEECA", "Đèn bàn", "4.2", 449000));
+            productList.add(new Product(R.drawable.img_densan_logly,"LOGLY", "Đèn sàn", "4.4", 990000));
             productList.add(new Product(R.drawable.img_dentrangtri_firefly,"FIREFLY", "Đèn trang trí", "4.7", 199000));
-            productList.add(new Product(R.drawable.img_densan_noti,"NOTI", "Đèn sàn", "4.7", 990000));
-            productList.add(new Product(R.drawable.img_denban_quiin,"QUIIN", "Đèn bàn", "4.7", 399000));
-            productList.add(new Product(R.drawable.img_dentrangtri_lampy,"LAMPY", "Đèn trang trí", "4.7", 245000));
+            productList.add(new Product(R.drawable.img_densan_noti,"NOTI", "Đèn sàn", "4.5", 990000));
+            productList.add(new Product(R.drawable.img_denban_quiin,"QUIIN", "Đèn bàn", "4.9", 399000));
+            productList.add(new Product(R.drawable.img_dentrangtri_lampy,"LAMPY", "Đèn trang trí", "4.6", 245000));
             binding.txtCategoryName.setText("Đèn");
         }
         else if (Objects.equals(category, "ketu")){
             productList = new ArrayList<>();
-            productList.add(new Product(R.drawable.img_kedungdo_ez,"EZ", "Kệ đựng đồ", "4.7", 490000));
-            productList.add(new Product(R.drawable.img_ketugiay_johy,"JOHY", "Kệ, tủ giày", "4.7", 2850000));
-            productList.add(new Product(R.drawable.img_ketuquanao_quada,"QUADA", "Kệ, tử quần áo", "4.7", 22200000));
+            productList.add(new Product(R.drawable.img_kedungdo_ez,"EZ", "Kệ đựng đồ", "4.6", 490000));
+            productList.add(new Product(R.drawable.img_ketugiay_johy,"JOHY", "Kệ, tủ giày", "4.2", 2850000));
+            productList.add(new Product(R.drawable.img_ketuquanao_quada,"QUADA", "Kệ, tử quần áo", "4.0", 22200000));
             productList.add(new Product(R.drawable.img_ketugiay_lam,"LAM", "Kệ, tủ giày", "4.7", 1920000));
-            productList.add(new Product(R.drawable.img_ketuquanao_tasota,"TASOTA", "Kệ, tử quần áo", "4.7", 485000));
-            productList.add(new Product(R.drawable.img_ketutv_hobu,"HOBU", "Kệ, tủ TV", "4.7", 2800000));
+            productList.add(new Product(R.drawable.img_ketuquanao_tasota,"TASOTA", "Kệ, tử quần áo", "4.1", 485000));
+            productList.add(new Product(R.drawable.img_ketutv_hobu,"HOBU", "Kệ, tủ TV", "4.9", 2800000));
             binding.txtCategoryName.setText("Kệ tủ");
         }
         else if (Objects.equals(category, "trangtri")){
             productList = new ArrayList<>();
-            productList.add(new Product(R.drawable.img_ttdongho_queen,"QUEEN", "Đồng hồ", "4.7", 799000));
-            productList.add(new Product(R.drawable.img_tttranhanh_moon,"MOON", "Tranh ảnh", "4.7", 199000));
-            productList.add(new Product(R.drawable.img_ttvanphongpham_khaydungghichu,"Khay đựng ghi chú", "Văn phòng phẩm", "4.7", 59000));
-            productList.add(new Product(R.drawable.img_ttdongho_king,"King", "Đồng hồ", "4.7", 899000));
-            productList.add(new Product(R.drawable.img_tttranhanh_you,"YOU", "Tranh ảnh", "4.7", 199000));
-            productList.add(new Product(R.drawable.img_ttvanphongpham_ongdungbut,"Ống đựng bút", "Văn phòng phẩm", "4.7", 59000));
+            productList.add(new Product(R.drawable.img_ttdongho_queen,"QUEEN", "Đồng hồ", "4.0", 799000));
+            productList.add(new Product(R.drawable.img_tttranhanh_moon,"MOON", "Tranh ảnh", "4.1", 199000));
+            productList.add(new Product(R.drawable.img_ttvanphongpham_khaydungghichu,"FIJI", "Khay đựng ghi chú", "4.7", 59000));
+            productList.add(new Product(R.drawable.img_ttdongho_king,"King", "Đồng hồ", "4.8", 899000));
+            productList.add(new Product(R.drawable.img_tttranhanh_you,"YOU", "Tranh ảnh", "4.4", 199000));
+            productList.add(new Product(R.drawable.img_ttvanphongpham_ongdungbut,"TAKA", "Ống đựng bút", "3.5", 59000));
             binding.txtCategoryName.setText("Trang trí");
         }
         else  if (Objects.equals(category, "giuong")){
             productList = new ArrayList<>();
-            productList.add(new Product(R.drawable.img_giuongngu_hano,"HANO", "Giường ngủ", "4.7", 13950000));
-            productList.add(new Product(R.drawable.img_nemngoi_lollipop,"LOLLIPOP", "Nệm ngồi", "4.7", 169000));
-            productList.add(new Product(R.drawable.img_nemcaosu_springvenus,"SPRING VENUS", "Nệm cao su", "4.7", 7314000));
-            productList.add(new Product(R.drawable.img_giuongngu_lullaby,"LULLABY", "Giường ngủ", "4.7", 10500000));
+            productList.add(new Product(R.drawable.img_giuongngu_hano,"HANO", "Giường ngủ", "4.6", 13950000));
+            productList.add(new Product(R.drawable.img_nemngoi_lollipop,"LOLLIPOP", "Nệm ngồi", "4.8", 169000));
+            productList.add(new Product(R.drawable.img_nemcaosu_springvenus,"SPRING VENUS", "Nệm cao su", "4.9", 7314000));
+            productList.add(new Product(R.drawable.img_giuongngu_lullaby,"LULLABY", "Giường ngủ", "4.2", 10500000));
             productList.add(new Product(R.drawable.img_nemngoi_candy,"CANDY", "Nệm ngồi", "4.7", 199000));
-            productList.add(new Product(R.drawable.img_giuongngu_dalatgrace,"DALAT GRACE", "Giường ngủ", "4.7", 9850000));
+            productList.add(new Product(R.drawable.img_giuongngu_dalatgrace,"DALAT GRACE", "Giường ngủ", "4.1", 9850000));
             binding.txtCategoryName.setText("Giường");
         }
 
